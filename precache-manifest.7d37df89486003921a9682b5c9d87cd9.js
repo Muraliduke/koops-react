3a0fb034e67cf7c72d3c72c070bfc0371d5b3ce9
Murali <muraliduke4461@gmail.com>
2020-09-07 13:33:22 +0530

    Add files via upload
    Initial commit

---
+++ precache-manifest.7d37df89486003921a9682b5c9d87cd9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396bfa8032b236e0a790240713e24beb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3225531850616184acc5",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "83a19a9a43650347bb66",
+    "url": "/static/css/main.af5a7b48.chunk.css"
+  },
+  {
+    "revision": "3225531850616184acc5",
+    "url": "/static/js/2.58ae1a7e.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.58ae1a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a19a9a43650347bb66",
+    "url": "/static/js/main.6711652c.chunk.js"
+  },
+  {
+    "revision": "3e18ffe207e88c49a388",
+    "url": "/static/js/runtime-main.16cf4d7c.js"
+  },
+  {
+    "revision": "34ea2ffe13fc4e2ce3f19502e16e9e1c",
+    "url": "/static/media/HouseCleaning.34ea2ffe.jpg"
+  },
+  {
+    "revision": "7ffb289e99b04362007f96a4046021e3",
+    "url": "/static/media/calendar.7ffb289e.svg"
+  },
+  {
+    "revision": "9d640f7908f1999775064ec890cff9eb",
+    "url": "/static/media/clean.9d640f79.mp4"
+  },
+  {
+    "revision": "1129b0ca4ea8ae89158927cf486b1d54",
+    "url": "/static/media/convenient.1129b0ca.svg"
+  },
+  {
+    "revision": "aae3d0ca12d61ca671871f74c112bd06",
+    "url": "/static/media/experienced.aae3d0ca.svg"
+  },
+  {
+    "revision": "cc77b5ec4aaed3eaa4f4c930b86922b4",
+    "url": "/static/media/flexible.cc77b5ec.svg"
+  },
+  {
+    "revision": "d0637e11ab9f19b4b7f7147fbf5ae3d5",
+    "url": "/static/media/mobile.d0637e11.svg"
+  },
+  {
+    "revision": "a2846cd770620de56f04ec8abc3f5988",
+    "url": "/static/media/relax.a2846cd7.svg"
+  },
+  {
+    "revision": "7470fd8d9fb627a08ca8ca73a9d0a469",
+    "url": "/static/media/reliable.7470fd8d.svg"
+  }
+]);
